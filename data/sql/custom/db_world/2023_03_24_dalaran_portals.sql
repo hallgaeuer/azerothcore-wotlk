4e8a94be2ab756a31bc3542af2e7d796ecb42944
Hasn <h.allgaeuer@hotmail.de>
2023-03-24 19:45:51 +0100

    Portals to Dalaran in Stormwind and Orgrimmar

---
+++ data/sql/custom/db_world/2023_03_24_dalaran_portals.sql
@@ -0,0 +1,13 @@
+/* Stormwind */
+INSERT IGNORE INTO gameobject (guid, id, map, zoneId, areaId, spawnMask, phaseMask, position_x, position_y, position_z,
+                               orientation, rotation0, rotation1, rotation2, rotation3, spawntimesecs, animprogress,
+                               state, ScriptName, VerifiedBuild)
+VALUES (2535479, 202079, 0, 0, 0, 1, 1, -8844.51, 629.318, 94.8679, 3.36122, -0, -0, -0.993977, 0.109593, 300, 0, 1, '',
+        null);
+
+/* Orgrimmar */
+INSERT IGNORE INTO gameobject (guid, id, map, zoneId, areaId, spawnMask, phaseMask, position_x, position_y,
+                                    position_z, orientation, rotation0, rotation1, rotation2, rotation3, spawntimesecs,
+                                    animprogress, state, ScriptName, VerifiedBuild)
+VALUES (2535480, 202079, 1, 0, 0, 1, 1, 1584.05, -4415.73, 8.33543, 0.276884, -0, -0, -0.138, -0.990432, 300, 0, 1, '',
+        null);

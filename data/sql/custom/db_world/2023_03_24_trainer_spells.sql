c917f6ee273a3d085532ae155e794dcab2fc20b8
Hasn <h.allgaeuer@hotmail.de>
2023-03-25 17:24:14 +0100

    Additional trainer spells by ID instead of name

--- data/sql/custom/db_world/2023_03_24_trainer_spells.sql
+++ data/sql/custom/db_world/2023_03_24_trainer_spells.sql
@@ -3,38 +3,38 @@
 /**********************/
 /* Defensive Stance */
 INSERT IGNORE INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) 
-SELECT `creature_template`.`entry`, 71, 0, 0, 0, 0 FROM `creature_template` WHERE (`subname` LIKE '%Warrior Trainer%');
+SELECT `creature_template`.`entry`, 71, 0, 0, 0, 0 FROM `creature_template` WHERE `trainer_class` = 1;
 
 /* Berserker Stance */
 INSERT IGNORE INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) 
-SELECT `creature_template`.`entry`, 2458, 0, 0, 0, 0 FROM `creature_template` WHERE (`subname` LIKE '%Warrior Trainer%');
+SELECT `creature_template`.`entry`, 2458, 0, 0, 0, 0 FROM `creature_template` WHERE `trainer_class` = 1;
 
 /* Taunt */
 INSERT IGNORE INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) 
-SELECT `creature_template`.`entry`, 355, 0, 0, 0, 0 FROM `creature_template` WHERE (`subname` LIKE '%Warrior Trainer%');
+SELECT `creature_template`.`entry`, 355, 0, 0, 0, 0 FROM `creature_template` WHERE `trainer_class` = 1;
 
 /* Sunder Armor */
 INSERT IGNORE INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) 
-SELECT `creature_template`.`entry`, 7386, 0, 0, 0, 0 FROM `creature_template` WHERE (`subname` LIKE '%Warrior Trainer%');
+SELECT `creature_template`.`entry`, 7386, 0, 0, 0, 0 FROM `creature_template` WHERE `trainer_class` = 1;
 
 /* Intercept */
 INSERT IGNORE INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) 
-SELECT `creature_template`.`entry`, 20252, 0, 0, 0, 0 FROM `creature_template` WHERE (`subname` LIKE '%Warrior Trainer%');
+SELECT `creature_template`.`entry`, 20252, 0, 0, 0, 0 FROM `creature_template` WHERE `trainer_class` = 1;
 
 /**********************/
 /* Warlock */
 /**********************/
 /* Summon Succubus */
 INSERT IGNORE INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) 
-SELECT `creature_template`.`entry`, 712, 0, 0, 0, 0 FROM `creature_template` WHERE (`subname` LIKE '%Warlock Trainer%');
+SELECT `creature_template`.`entry`, 712, 0, 0, 0, 0 FROM `creature_template` WHERE `trainer_class` = 9;
 
 /* Summon Voidwalker */
 INSERT IGNORE INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) 
-SELECT `creature_template`.`entry`, 697, 0, 0, 0, 0 FROM `creature_template` WHERE (`subname` LIKE '%Warlock Trainer%');
+SELECT `creature_template`.`entry`, 697, 0, 0, 0, 0 FROM `creature_template` WHERE `trainer_class` = 9;
 
 /* Summon Felhunter */
 INSERT IGNORE INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqSkillLine`, `ReqSkillRank`, `ReqLevel`) 
-SELECT `creature_template`.`entry`, 691, 0, 0, 0, 0 FROM `creature_template` WHERE (`subname` LIKE '%Warlock Trainer%');
+SELECT `creature_template`.`entry`, 691, 0, 0, 0, 0 FROM `creature_template` WHERE `trainer_class` = 9;
 
 /**********************/
 /* Death Knight */

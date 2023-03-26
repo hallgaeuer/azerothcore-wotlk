508612ae2f456fa08564c3bf1f3b0943a592e49c
Hasn <h.allgaeuer@hotmail.de>
2023-03-26 23:17:41 +0200

    Initial lvl60 5 man raid balancing

---
+++ data/sql/custom/db_world/2023_03_26_raid_balancing.sql
@@ -0,0 +1,7 @@
+truncate table mod_quickbalance_modifier;
+insert into mod_quickbalance_modifier (Map, Creature, DamageModifier, HealthModifier, ManaModifier, ArmorModifier, Comment)
+values  (309, 0, 0.7, 0.6, 1, 1, 'Zul Gurub'),
+        (409, 0, 0.7, 0.2, 1, 1, 'Molten Core'),
+        (469, 0, 0.7, 0.2, 1, 1, 'BWL'),
+        (509, 0, 0.7, 0.4, 1, 1, 'AQ20'),
+        (531, 0, 0.7, 0.2, 1, 1, 'Molten Core');

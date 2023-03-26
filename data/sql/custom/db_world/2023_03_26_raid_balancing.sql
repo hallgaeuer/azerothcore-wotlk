fafe8f610827268234380ee34e171be4fef01edd
Hasn <h.allgaeuer@hotmail.de>
2023-03-26 23:43:03 +0200

    Update raid balancing

--- data/sql/custom/db_world/2023_03_26_raid_balancing.sql
+++ data/sql/custom/db_world/2023_03_26_raid_balancing.sql
@@ -1,7 +1,7 @@
-truncate table mod_quickbalance_modifier;
 insert into mod_quickbalance_modifier (Map, Creature, DamageModifier, HealthModifier, ManaModifier, ArmorModifier, Comment)
 values  (309, 0, 0.7, 0.6, 1, 1, 'Zul Gurub'),
         (409, 0, 0.7, 0.2, 1, 1, 'Molten Core'),
+        (409, 12099, 0.1, 0.2, 1, 1, 'Molten Core - Firesworn (Garr)'),
         (469, 0, 0.7, 0.2, 1, 1, 'BWL'),
         (509, 0, 0.7, 0.4, 1, 1, 'AQ20'),
-        (531, 0, 0.7, 0.2, 1, 1, 'Molten Core');
+        (531, 0, 0.7, 0.2, 1, 1, 'AQ40');

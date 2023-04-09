9e757a6bf9345786f3a60d2df16183013a29f70a
Hasn <h.allgaeuer@hotmail.de>
2023-04-09 19:47:14 +0200

    No Hearthstone cooldown

---
+++ data/sql/custom/db_world/items/no_hearthstone_cd.sql
@@ -0,0 +1,12 @@
+UPDATE `item_template`
+SET `spellcooldown_1`         = 0,
+    `spellcooldown_2`         = 0,
+    `spellcooldown_3`         = 0,
+    `spellcooldown_4`         = 0,
+    `spellcooldown_5`         = 0,
+    `spellcategorycooldown_1` = 0,
+    `spellcategorycooldown_2` = 0,
+    `spellcategorycooldown_3` = 0,
+    `spellcategorycooldown_4` = 0,
+    `spellcategorycooldown_5` = 0
+WHERE (`entry` = 6948);

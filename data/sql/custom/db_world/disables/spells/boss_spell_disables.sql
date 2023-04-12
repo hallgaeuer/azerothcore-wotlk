d06383c55464ee38383ae0d806af5165cf1069f7
Hasn <h.allgaeuer@hotmail.de>
2023-04-12 23:07:23 +0200

    Fix creature spell disables

--- data/sql/custom/db_world/disables/spells/boss_spell_disables.sql
+++ data/sql/custom/db_world/disables/spells/boss_spell_disables.sql
@@ -1,8 +1,8 @@
 REPLACE INTO disables (sourceType, entry, flags, params_0, params_1, comment) VALUES
-	(0, 25646, 8, 0, 0, 'Mortal Wound Tankswap Spell (Kurinnax, Fankriss, Gluth)'),
-    (0, 13879, 8, 0, 0, 'MC Golemagg: Disable Lava Splash'),
-    (0, 58666, 8, 0, 0, 'Archavon 10: Disable Impale'),
-    (0, 60882, 8, 0, 0, 'Archavon 25: Disable Impale'),
-    (0, 71127, 8, 0, 0, 'ICC: Stinky / Precious: Disable Mortal Wound'),
-    (0, 72219, 8, 0, 0, 'ICC: Festergut: Disable Gastric Bloat')
+	(0, 25646, 2, 0, 0, 'Mortal Wound Tankswap Spell (Kurinnax, Fankriss, Gluth)'),
+    (0, 13879, 2, 0, 0, 'MC Golemagg: Disable Lava Splash'),
+    (0, 58666, 2, 0, 0, 'Archavon 10: Disable Impale'),
+    (0, 60882, 2, 0, 0, 'Archavon 25: Disable Impale'),
+    (0, 71127, 2, 0, 0, 'ICC: Stinky / Precious: Disable Mortal Wound'),
+    (0, 72219, 2, 0, 0, 'ICC: Festergut: Disable Gastric Bloat')
 ;

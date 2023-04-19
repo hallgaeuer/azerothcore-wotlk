68eb48ab7477c6e8b01b510c558818ffac89534b
Hasn <h.allgaeuer@hotmail.de>
2023-04-19 23:54:32 +0200

    Disable ICC dungeons for now

---
+++ data/sql/custom/db_world/dungeon/disable_icc_dungeons.sql
@@ -0,0 +1,6 @@
+REPLACE INTO disables (sourceType, entry, flags, params_0, params_1, comment)
+VALUES
+    (2, 632, 3, "", "", "The Forge of Souls"),
+    (2, 668, 3, "", "", "Halls of Reflection"),
+    (2, 658, 3, "", "", "Pit of Saron")
+;

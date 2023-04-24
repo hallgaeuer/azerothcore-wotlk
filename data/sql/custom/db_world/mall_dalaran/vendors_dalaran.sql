6bfce7cb9f7517d87cbef20264a6d245011d69b1
Hasn <h.allgaeuer@hotmail.de>
2023-04-24 19:46:13 +0200

    Fix heirloom vendor position

--- data/sql/custom/db_world/mall_dalaran/vendors_dalaran.sql
+++ data/sql/custom/db_world/mall_dalaran/vendors_dalaran.sql
@@ -1,12 +1,8 @@
 /* Spawn Heirloom vendor at Krasus Landing */
-DELETE FROM creature WHERE guid = 5000551;
-INSERT INTO creature
-    (guid, id1, id2, id3, map, zoneId, areaId, spawnMask, phaseMask, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, wander_distance, currentwaypoint, curhealth, curmana, MovementType, npcflag, unit_flags, dynamicflags, ScriptName, VerifiedBuild) VALUES
-    (5000551, 35507, 0, 0, 571, 0, 0, 1, 1, 1, 5788.47, 483.833, 657.605, 5.49383, 300, 0, 0, 10080, 8814, 0, 0, 0, 0, '', null);
+REPLACE INTO creature (guid, id1, id2, id3, map, zoneId, areaId, spawnMask, phaseMask, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, wander_distance, currentwaypoint, curhealth, curmana, MovementType, npcflag, unit_flags, dynamicflags, ScriptName, VerifiedBuild) values (5000551, 35507, 0, 0, 571, 0, 0, 1, 1, 1, 5785.84, 480.659, 657.603, 5.57005, 300, 0, 0, 10080, 8814, 0, 0, 0, 0, '', null);
 
 /* Spawn lvl 80 green gear vendors in Dalaran Krasus Landing */
-DELETE FROM creature WHERE guid BETWEEN 5000567 AND 5000572;
-insert into creature (guid, id1, id2, id3, map, zoneId, areaId, spawnMask, phaseMask, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, wander_distance, currentwaypoint, curhealth, curmana, MovementType, npcflag, unit_flags, dynamicflags, ScriptName, VerifiedBuild)
+REPLACE into creature (guid, id1, id2, id3, map, zoneId, areaId, spawnMask, phaseMask, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, wander_distance, currentwaypoint, curhealth, curmana, MovementType, npcflag, unit_flags, dynamicflags, ScriptName, VerifiedBuild)
 values
         (5000572, 9000007, 0, 0, 571, 0, 0, 1, 1, 0, 5787.51, 449.478, 658.782, 0.103625, 300, 0, 0, 5342, 0, 0, 0, 0, 0, '', null),
         (5000571, 9000006, 0, 0, 571, 0, 0, 1, 1, 0, 5787.69, 451.196, 658.783, 6.12371, 300, 0, 0, 5342, 0, 0, 0, 0, 0, '', null),
@@ -17,6 +13,13 @@ values
 
 REPLACE INTO creature (guid, id1, id2, id3, map, zoneId, areaId, spawnMask, phaseMask, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, wander_distance, currentwaypoint, curhealth, curmana, MovementType, npcflag, unit_flags, dynamicflags, ScriptName, VerifiedBuild)
 values  (5000590, 9000009, 0, 0, 571, 0, 0, 1, 1, 0, 5787.57, 447.779, 658.783, 0.102936, 300, 0, 0, 5342, 0, 0, 0, 0, 0, '', null);
+
+/* Reagents */
+REPLACE INTO creature (guid, id1, id2, id3, map, zoneId, areaId, spawnMask, phaseMask, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, wander_distance, currentwaypoint, curhealth, curmana, MovementType, npcflag, unit_flags, dynamicflags, ScriptName, VerifiedBuild) VALUES (5000557, 28714, 0, 0, 571, 0, 0, 1, 1, 0, 5787.18, 482.429, 657.604, 5.55754, 300, 0, 0, 8508, 7981, 0, 0, 0, 0, '', null);
+
+/* Ammo */
+REPLACE into creature (guid, id1, id2, id3, map, zoneId, areaId, spawnMask, phaseMask, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, wander_distance, currentwaypoint, curhealth, curmana, MovementType, npcflag, unit_flags, dynamicflags, ScriptName, VerifiedBuild)
+values  (5000575, 29493, 0, 0, 571, 0, 0, 1, 1, 0, 5788.28, 483.771, 657.606, 5.59828, 300, 0, 0, 10635, 0, 0, 0, 0, 0, '', null);
 
 /* Auction Master */
 REPLACE INTO creature (guid, id1, id2, id3, map, zoneId, areaId, spawnMask, phaseMask, equipment_id, position_x, position_y, position_z, orientation, spawntimesecs, wander_distance, currentwaypoint, curhealth, curmana, MovementType, npcflag, unit_flags, dynamicflags, ScriptName, VerifiedBuild) values (5000593, 8661, 0, 0, 571, 0, 0, 1, 1, 1, 5849.38, 447.174, 658.816, 3.1244, 300, 0, 0, 6645, 0, 0, 0, 0, 0, '', null);

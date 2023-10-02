fd0ef140daf490a86255c15ea91a3fcf3cbc8e75
hasn <h.allgaeuer@hotmail.de>
2023-10-02 11:08:30 +0200

    Gear vendors: Removed some superflous items

--- data/sql/custom/db_world/vendors/green_gear_vendors.sql
+++ data/sql/custom/db_world/vendors/green_gear_vendors.sql
@@ -10,7 +10,27 @@ values  (@vendorId, 0, 0, 0, 0, 0, 2029, 0, 0, 0, 'Gear Vendor', 'Plate armor', 
 
 DELETE FROM npc_vendor WHERE entry = @vendorId;
 INSERT INTO npc_vendor (entry, slot, item)
-SELECT @vendorId, InventoryType, entry FROM item_template WHERE ItemLevel = 174 AND RandomSuffix = 0 AND Quality = 2 AND AllowableClass IN (-1, 32767, 262143) AND AllowableRace IN (-1, 2147483647) AND class = 4 AND subclass = 4 AND entry NOT IN (43938, 43848, 42885);
+SELECT @vendorId, InventoryType, entry FROM item_template WHERE ItemLevel = 174 AND RandomSuffix = 0 AND Quality = 2 AND AllowableClass IN (-1, 32767, 262143) AND AllowableRace IN (-1, 2147483647) AND class = 4 AND subclass = 4 AND entry NOT IN (
+    43938, 43848, 42885, -- Invalid items
+    -- Superflous items
+    42878,
+    42879,
+    43941,
+    43946,
+    39458,
+    42826,
+    43947,
+    42832,
+    42836,
+    39102,
+    43930,
+    39100,
+    43942,
+    43944,
+    42880,
+    43842,
+    42834
+);
 
 /*------------------------------*/
 /* Green mail armor vendor */
@@ -22,7 +42,34 @@ values  (@vendorId, 0, 0, 0, 0, 0, 2029, 0, 0, 0, 'Gear Vendor', 'Mail armor', '
 
 DELETE FROM npc_vendor WHERE entry = @vendorId;
 INSERT INTO npc_vendor (entry, slot, item)
-SELECT @vendorId, InventoryType, entry FROM item_template WHERE ItemLevel = 174 AND RandomSuffix = 0 AND Quality = 2 AND AllowableClass IN (-1, 32767, 262143) AND AllowableRace = -1  AND class = 4 AND subclass = 3 AND entry NOT IN (42886, 43936);
+SELECT @vendorId, InventoryType, entry FROM item_template WHERE ItemLevel = 174 AND RandomSuffix = 0 AND Quality = 2 AND AllowableClass IN (-1, 32767, 262143) AND AllowableRace = -1  AND class = 4 AND subclass = 3 AND entry NOT IN (
+    42886, 43936, -- Invalid items
+    -- Superflous items
+    42810,
+    42895,
+    43982,
+    42887,
+    42820,
+    43977,
+    42896,
+    42814,
+    42818,
+    42889,
+    42821,
+    42817,
+    43932,
+    39887,
+    39066,
+    43931,
+    43948,
+    39068,
+    42888,
+    43934,
+    43980,
+    39070,
+    43933,
+    43981
+);
 
 /*------------------------------*/
 /* Green leather armor vendor */
@@ -34,7 +81,32 @@ values  (@vendorId, 0, 0, 0, 0, 0, 2029, 0, 0, 0, 'Gear Vendor', 'Leather armor'
 
 DELETE FROM npc_vendor WHERE entry = @vendorId;
 INSERT INTO npc_vendor (entry, slot, item)
-SELECT @vendorId, InventoryType, entry FROM item_template WHERE ItemLevel = 174 AND RandomSuffix = 0 AND Quality = 2 AND AllowableClass IN (-1, 32767, 262143) AND AllowableRace = -1 AND class = 4 AND subclass = 2 AND entry NOT IN (43895, 42875);
+SELECT @vendorId, InventoryType, entry FROM item_template WHERE ItemLevel = 174 AND RandomSuffix = 0 AND Quality = 2 AND AllowableClass IN (-1, 32767, 262143) AND AllowableRace = -1 AND class = 4 AND subclass = 2 AND entry NOT IN (
+    43895, 42875,
+    -- Superflous items
+    43840,
+    43907,
+    42869,
+    43906,
+    43912,
+    39036,
+    42803,
+    43892,
+    43909,
+    42867,
+    39035,
+    42874,
+    43894,
+    43908,
+    39038,
+    42801,
+    42806,
+    43913,
+    39040,
+    42809,
+    43904,
+    43910
+);
 
 /*------------------------------*/
 /* Green cloth armor vendor */
@@ -46,7 +118,28 @@ values  (@vendorId, 0, 0, 0, 0, 0, 2029, 0, 0, 0, 'Gear Vendor', 'Cloth armor an
 
 DELETE FROM npc_vendor WHERE entry = @vendorId;
 INSERT INTO npc_vendor (entry, slot, item)
-SELECT @vendorId, InventoryType, entry FROM item_template WHERE ItemLevel = 174 AND RandomSuffix = 0 AND Quality = 2 AND AllowableClass IN (-1, 32767, 262143) AND AllowableRace = -1 AND class = 4 AND subclass = 1 AND entry NOT IN (43878);
+SELECT @vendorId, InventoryType, entry FROM item_template WHERE ItemLevel = 174 AND RandomSuffix = 0 AND Quality = 2 AND AllowableClass IN (-1, 32767, 262143) AND AllowableRace = -1 AND class = 4 AND subclass = 1 AND entry NOT IN (
+    43878,
+    -- Superflous items
+    42843,
+    42850,
+    43882,
+    43862,
+    43872,
+    42788,
+    42795,
+    42841,
+    42846,
+    42789,
+    43839,
+    43879,
+    38736,
+    43855,
+    43856,
+    38741,
+    43881,
+    42792
+);
 
 /*------------------------------*/
 /* Green weapon vendor */

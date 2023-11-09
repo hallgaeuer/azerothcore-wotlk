43a00997785d11c8397ba91289f93429ccda36cb
hasn <h.allgaeuer@hotmail.de>
2023-11-09 19:24:01 +0100

    Simplify 25m loot in 10m query, copied skip_dk_script.sql from module so that it actually gets executed automatically

--- data/sql/custom/db_world/items/copy_25m_loot_to_10m.sql
+++ data/sql/custom/db_world/items/copy_25m_loot_to_10m.sql
@@ -1,31 +1,9 @@
 /* Adds heroic (25m) loot to all non heroic mobs (10m). FIXME: Currently queries are way too wide and also affect heroic and non-heroic dungeons */
-DELETE FROM creature_loot_template WHERE entry IN (
-	SELECT Entry FROM creature_template WHERE creature_template.difficulty_entry_1 > 0
-);
-
-INSERT INTO creature_loot_template (Entry, Item, Reference, Chance, QuestRequired, LootMode, GroupId, MinCount, MaxCount, Comment)
-SELECT 
-	creature_template.entry , 
-	creature_loot_template.Item, 
-	creature_loot_template.Reference, 
-	creature_loot_template.Chance, 
-	creature_loot_template.QuestRequired, 
-	creature_loot_template.LootMode, 
-	creature_loot_template.GroupId, 
-	creature_loot_template.MinCount, 
-	creature_loot_template.MaxCount, 
-	creature_loot_template.Comment 
-FROM 
-	creature_loot_template 
-	INNER JOIN creature_template ON creature_template.difficulty_entry_1 = creature_loot_template.Entry 
-WHERE creature_template.difficulty_entry_1 > 0;
-
 UPDATE 
-    creature_template
-    INNER JOIN creature_template creature_template_diff_1 ON creature_template.difficulty_entry_1 = creature_template_diff_1.entry
-SET 
-    creature_template.lootid = creature_template_diff_1.lootid;
-
+	creature_template 
+	INNER JOIN creature_template creature_template_diff_1 ON creature_template.difficulty_entry_1 = creature_template_diff_1.entry
+SET creature_template.lootid = creature_template_diff_1.lootid 
+WHERE creature_template.difficulty_entry_1 > 0;
 
 /* Copy over Chests */ 
 /* Four Horsemen Chest */

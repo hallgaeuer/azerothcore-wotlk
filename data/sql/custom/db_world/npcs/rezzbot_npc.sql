7172473a09b1bf4b0c1e0b46862e5d9ebb0d60ac
hasn <h.allgaeuer@hotmail.de>
2023-11-13 22:14:08 +0100

    Increase visibility range of rezbot

--- data/sql/custom/db_world/npcs/rezzbot_npc.sql
+++ data/sql/custom/db_world/npcs/rezzbot_npc.sql
@@ -1,3 +1,7 @@
 DELETE FROM `creature_template` WHERE (`entry` = 9000011);
 INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_flight`, `detection_range`, `scale`, `rank`, `dmgschool`, `DamageModifier`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `spell_school_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES
 (9000011, 0, 0, 0, 0, 0, 5233, 0, 0, 0, 'Rezbot', '', '', 0, 80, 80, 0, 35, 1, 1, 1.14286, 1, 1, 1, 0.5, 0, 0, 1, 0, 0, 0, 0, 8, 131, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, '', 0);
+
+DELETE FROM `creature_template_addon` WHERE (`entry` = 9000011);
+INSERT INTO `creature_template_addon` (`entry`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `visibilityDistanceType`, `auras`) VALUES
+(9000011, 0, 0, 0, 0, 0, 5, '');

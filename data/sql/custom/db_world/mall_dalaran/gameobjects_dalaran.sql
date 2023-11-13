ef2acbf4b315cfeeafd1de0d6ecafb5d794d1dda
hasn <h.allgaeuer@hotmail.de>
2023-11-13 16:25:39 +0100

    Fix Runeforge in Dalaran

--- data/sql/custom/db_world/mall_dalaran/gameobjects_dalaran.sql
+++ data/sql/custom/db_world/mall_dalaran/gameobjects_dalaran.sql
@@ -2,5 +2,5 @@
 replace into gameobject (guid, id, map, zoneId, areaId, spawnMask, phaseMask, position_x, position_y,
                                     position_z, orientation, rotation0, rotation1, rotation2, rotation3, spawntimesecs,
                                     animprogress, state, ScriptName, VerifiedBuild)
-values (7535517, 191746, 571, 0, 0, 1, 1, 5842.3, 494.116, 657.125, 3.36542, -0, -0, -0.993744, 0.111678, 300, 0, 1, '',
+values (7535517, 191758, 571, 0, 0, 1, 1, 5842.3, 494.116, 657.125, 3.36542, -0, -0, -0.993744, 0.111678, 300, 0, 1, '',
         null);
